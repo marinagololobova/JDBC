d6e36b07affb9448739bbc4ad87c1ace268a0e92
marinagololobova <marinagololobova@mail.ru>
2023-04-06 23:35:03 +0300

    JDBC

---
+++ src/main/java/DAO/Employee.java
@@ -0,0 +1,82 @@
+package DAO;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private int age;
+    private City city;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName, String gender, int age, City city) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.age = age;
+        this.city = city;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", age=" + age +
+                ", cityId='" + city + '\'' +
+                '}';
+    }
+}
